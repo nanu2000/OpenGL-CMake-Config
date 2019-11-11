1ab94481f95839fa6e70185d818c2b4c4b7fe5b6
Richard sikra <richiesikra@gmail.com>
2019-11-10 19:01:41 -0800

    Updated CMake

---
+++ GNSConfig.cmake
@@ -0,0 +1,2 @@
+set(GNS_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/lib/gamenetworksockets/include")
+set(GNS_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/gamenetworksockets_build/Debug/x86/src/GameNetworkingSockets.lib")
